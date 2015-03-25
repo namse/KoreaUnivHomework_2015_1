6c3430558f9ea27f804f504654f4348ddae3893f
skatpgusskat <skatpgusskat@naver.com>
2015-03-26 00:44:48 +0900

    code on

--- System Design/hw2/hw2.asm
+++ System Design/hw2/hw2.asm
@@ -10,52 +10,53 @@ FLDE	DW	0F120H
 FLDF	DW	?
 ; ----------------------------------------------------------
 MAIN	PROC	NEAR
-		MOV AX, CS
-		MOV DS, AX ; - init DS by CS
+		MOV	AX, CS
+		MOV	DS, AX		 ; - init DS by CS
 
-		MOV	AX, FLDD
-		ADD	AX, FLDE
-		MOV	FLDF, AX ; - FLDF = FLDD + FLDE
+		MOV 	AX, FLDD
+		ADD 	AX, FLDE
+		MOV 	FLDF, AX		 ; - FLDF = FLDD + FLDE
 
-		MOV	CX, 2 ; - 2 byte, 2 loop
-		LEA DI, FLDF
-		ADD DI, CX ; - for load byte of FLDF		
+		MOV 	CX, 2		 ; - 2 byte, 2 loop
+		LEA 	DI, FLDF
+		ADD 	DI, CX		 ; - for load byte of FLDF	
 A10PRINTLOOP:
-		DEC DI
-		MOV	DH, [DI] ; - load 1 byte of FLDF
-		MOV BX, 2 ; - 1 byte need 2 character, so, 2 print loop
-PRINTDL:
-		MOV	DL, 11110000B
-		AND	DL, DH ; - load upper 4 bit into DL
+		DEC 	DI
+		MOV	 DH, [DI]		 ; - load 1 byte of FLDF
+		MOV 	BX, 2 		 ; - 1 byte need 2 character, so, 2 print loop
+A20LOADDL:
+		MOV 	DL, 11110000B
+		AND 	DL, DH		 ; - load upper 4 bit into DL
 
-		MOV AH, CL
-		MOV CL, 4
-		SHR DL, CL
-		MOV	CL, AH ; - shift DL's bit to bottom
+		MOV 	AH, CL
+		MOV 	CL, 4
+		SHR 	DL, CL
+		MOV	CL, AH		 ; - shift DL's bit to bottom
 
-		CMP	DL, 10 ; - 0~9 or A~F
+		CMP	DL, 10		 ; - 0~9 or A~F
 		JB	ARABIT
 		JMP	ALPHABET
 ARABIT:
 		ADD	DL, 30H
-		JMP	PRINT
+		JMP	A30PRINTDL
 ALPHABET:
 		ADD	DL, 37H
-		JMP	PRINT
-PRINT:
+		JMP	A30PRINTDL
+A30PRINTDL:
 		MOV	AH, 02H
-		INT	21H ; - print DL character
+		INT	21H		 ; - print DL character
 
-		MOV AH, CL
-		MOV CL, 4
-		SHL DH, CL
-		MOV	CL, AH ; - shift DH left 4 bit to reuse above DL 4bit loadding codes. 
-
-		DEC BX
-		JNZ PRINTDL ; - loop for loading twise (2 character per 1 byte)
+		MOV 	AH, CL
+		MOV 	CL, 4
+		SHL 	DH, CL
+		MOV	CL, AH		; - shift DH left 4 bit to reuse
+						; above DL 4bit loadding codes. 
+		DEC 	BX
+		JNZ 	A20LOADDL	; - loop for loading twise 
+							;(2 character per 1 byte)
 
 		LOOP	A10PRINTLOOP
-OVER:
+
 		MOV	AX,	4C00H
 		INT	21H
 MAIN	ENDP
