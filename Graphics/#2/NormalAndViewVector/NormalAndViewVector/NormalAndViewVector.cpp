a56836c12a677cb39e7e8c44d70aea510ad3bb7c
Echo <skatpgusskat@naver.com>
2015-03-30 14:53:34 +0900

    add comment

--- Graphics/#2/NormalAndViewVector/NormalAndViewVector/NormalAndViewVector.cpp
+++ Graphics/#2/NormalAndViewVector/NormalAndViewVector/NormalAndViewVector.cpp
@@ -46,10 +46,14 @@ void Display()
 	glShadeModel(GL_FLAT);
 	glPointSize(5);
 
+
+	// Set Triangle
 	static Namse::Triangle triangle(Namse::Vector(-2, -2.5, -5),
 		Namse::Vector(2, -0.5, -5), Namse::Vector(0, 1.5, -5));
 	triangle.SetColor(1.0, 0, -5);
 
+
+	// For Rotation
 	static int previousTime = glutGet(GLUT_ELAPSED_TIME);
 	int currentTime = glutGet(GLUT_ELAPSED_TIME);
 	int dTime = currentTime - previousTime;
@@ -57,6 +61,8 @@ void Display()
 
 	triangle.RotateBy(0, 0.001 * dTime, 0);
 
+
+	// View Vector
 	static Namse::Vector ViewStartPoint(2, 0, -6);
 	static Namse::Vector ViewEndPoint(0, 0, -6);
 
@@ -64,14 +70,20 @@ void Display()
 	viewLine.SetColor(0, 1, 0);
 
 
+
+	// calculate Triangle's Normal Vector
 	auto centerOfTriangle = triangle.GetCenter();
 	auto triangleEdgeVector1 = triangle.m_Vector[1] - triangle.m_Vector[0];
 	auto triangleEdgeVector2 = triangle.m_Vector[2] - triangle.m_Vector[1];
 	auto normalVector = triangleEdgeVector2.CrossProduct(triangleEdgeVector1);
 	normalVector = normalVector.Unit();
 
+
+
 	Namse::Line normalVectorLine(centerOfTriangle, centerOfTriangle + normalVector * 2);
 	normalVectorLine.SetColor(1, 1, 0);
+
+
 
 	Namse::Line triangleEdgeLine1(triangle.m_Vector[0], triangle.m_Vector[1]);
 	Namse::Line triangleEdgeLine2(triangle.m_Vector[1], triangle.m_Vector[2]);
@@ -105,14 +117,20 @@ void Display()
 	GLdouble t = -(A * ViewStartPoint.m_X + B * ViewStartPoint.m_Y + C * ViewStartPoint.m_Z + D)
 		/ (A * viewVector.m_X + B * viewVector.m_Y + C * viewVector.m_Z);
 
+
+	// Intersection between triangle and viewVector
 	Namse::Vector intersectionPoint(viewVector.m_X * t + ViewStartPoint.m_X,
 		viewVector.m_Y * t + ViewStartPoint.m_Y,
 		viewVector.m_Z * t + ViewStartPoint.m_Z);
 
 	intersectionPoint.SetColor(0, 0, 1);
 
+
+	// cullling Test
 	if (normalVector.DotProduct(viewVector) < 0)
 		triangle.SetColor(0, 1, 0);
+
+
 
 	triangle.Draw();
 	viewLine.Draw();
